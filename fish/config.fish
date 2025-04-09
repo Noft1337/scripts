29b085b421ca7c706f4fc2b0ae73ffea4c06227f
Michael <101609620+Noft1337@users.noreply.github.com>
2025-04-09 11:56:13 +0300

    config.fish: aliases

--- fish/config.fish
+++ fish/config.fish
@@ -1,19 +1,24 @@
 if status is-interactive
     # Commands to run in interactive sessions can go here
 end
+
+# Aliases
 alias ll="ls -la"
+alias fish_reload="source ~/.config/fish/config.fish"
+alias fetch="git fetch --all --prune --tags --prune-tags"
+alias tmux-session="~/.local/scripts/tmux-sessioner.sh"
+alias timestamp="python3 -c 'from datetime import datetime; print(int(datetime.now().timestamp()))'"
 alias run_ubuntu_docker="sudo docker run -d --name=ubuntu -v /home/michael/Dockers/ubuntu_thinpro/data:/data ubuntu:latest sleep infinity"
+
+# Keybinds 
 bind \b backward-kill-word
 bind \e\[3\;3~ kill-word
 bind \e\[3\;5~ kill-word
 
-alias fish_reload="source ~/.config/fish/config.fish"
-
-
-alias tmux-session="/home/michael/.local/scripts/tmux-sessioner.sh"
-alias fish_reload="source ~/.config/fish/config.fish"
+# Random Env Vars
 set fish_conf "~/.config/fish/config.fish"
 
+# Tide Colored Prompt
 set tide_left_prompt_items vi_mode os context pwd python go git
 set tide_context_always_display true
 set tide_context_bg_color 333333
@@ -36,3 +41,11 @@ set tide_context_hostname_parts 1
 set tide_git_bg_color d7ff00
 set tide_git_bg_color_unstable f3005a
 set tide_git_bg_color_urgent 870000
+
+# Auto-set Default Python Env
+if test -d ./venv
+	# Set local env
+	source venv/bin/activate.fish
+else
+	source ~/envs/main/bin/activate.fish
+end
