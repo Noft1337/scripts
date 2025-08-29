6e4bdd6619caae6f77deb9aaf995b0580fe2378a
Noft1337 <maudri324@gmail.com>
2025-08-29 15:08:40 +0300

    fish: updated config.fish

--- fish/config.fish
+++ fish/config.fish
@@ -1,51 +1,45 @@
 if status is-interactive
     # Commands to run in interactive sessions can go here
 end
-
-# Aliases
-alias ll="ls -la"
-alias fish_reload="source ~/.config/fish/config.fish"
-alias fetch="git fetch --all --prune --tags --prune-tags"
-alias tmux-session="~/.local/scripts/tmux-sessioner.sh"
-alias timestamp="python3 -c 'from datetime import datetime; print(int(datetime.now().timestamp()))'"
+alias ll="ls -l"
 alias run_ubuntu_docker="sudo docker run -d --name=ubuntu -v /home/michael/Dockers/ubuntu_thinpro/data:/data ubuntu:latest sleep infinity"
-
-# Keybinds 
 bind \b backward-kill-word
 bind \e\[3\;3~ kill-word
 bind \e\[3\;5~ kill-word
 
-# Random Env Vars
+alias tmux-session="/home/michael/.local/scripts/tmux-sessioner.sh"
+alias fish_reload="source ~/.config/fish/config.fish"
 set fish_conf "~/.config/fish/config.fish"
 
-# Tide Colored Prompt
-set tide_left_prompt_items vi_mode os context pwd python go git
+set -g tide_left_prompt_items os context pwd kubectl python go git
+set -g tide_right_prompt_items
+# set tide_os_bg_color 4D4D4D
 set tide_context_always_display true
-set tide_context_bg_color 333333
-
-if  test (id -u) -eq 0
-	set tide_os_color FFFFFF
-	set tide_os_bg_color 980003
-	set tide_context_color_default ff0004
-	set tide_context_color_root 1fe300
-else
-	set tide_os_color 000000
-	set tide_os_bg_color 4D4D4D
-	set tide_context_color_default 1fe300
-	set tide_context_color_root ff0004
-end
-
-set tide_pwd_bg_color aaaaff
-set tide_context_hostname_parts 1
-
-set tide_git_bg_color d7ff00
+# set tide_context_bg_color 333333
+set tide_context_color_default 1fe300
+set tide_context_color_root f00000 
+set tide_context_hostname_prats 1 
+# if  test (id -u) -eq 0
+# 	set tide_pwd_bg_color 591111
+# end
+# 
+# set tide_git_bg_color d7ff00
 set tide_git_bg_color_unstable f3005a
 set tide_git_bg_color_urgent 870000
 
-# Auto-set Default Python Env
-if test -d ./venv
-	# Set local env
-	source venv/bin/activate.fish
-else
-	source ~/envs/main/bin/activate.fish
+set _pyenv_path "~/main/bin/activate.fish"
+set _pyenv_path_michael "~michael/main/bin/activate.fish"
+if test -f $_pyenv_path
+	source $_pyenv_path
+else if test -f $_pyenv_path_michael
+	source $_pyenv_path_michael
 end
+
+set STAGE_REL_NAME "pxc-db-stage"
+set PROD_REL_NAME "pxc-db-prod"
+set OPERATOR_REL_NAME "pxc-operator"
+# Namespaces
+set STAGE_NAMESPACE "percona-stage"
+set PROD_NAMESPACE "percona-prod"
+set OPERATOR_NAMESPACE "percona-operator"
+set BACKUP_NAME "backup-test-$(date +'%Y-%m-%d')"
